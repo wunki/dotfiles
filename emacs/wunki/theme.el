c29c2cefc9277ee2a73083a836c576bc2c1ac978
Petar Radosevic <wunki@ubuntu.(none)>
2011-07-17 06:20:14 -0700

    Unload solarized theme.

--- emacs/wunki/theme.el
+++ emacs/wunki/theme.el
@@ -2,7 +2,6 @@
 (setq color-theme-is-global t)
 
 (load-file "~/.emacs.d/elpa/color-theme-zenburn-0.3/color-theme-zenburn.el")
-(load-file "~/.emacs.d/elpa/color-theme-solarized-1.0.0/color-theme-solarized.elc")
 (color-theme-zenburn)
 
 ; set the default font

7501e4d03a851560495b317f491990ae7101fa89
Petar Radosevic <petar@wunki.org>
2011-09-11 13:31:00 +0200

    Theme cleanup

--- emacs/wunki/theme.el
+++ emacs/wunki/theme.el
@@ -1,13 +1,10 @@
-(require 'color-theme)
-(setq color-theme-is-global t)
-
-(load-file "~/.emacs.d/elpa/color-theme-zenburn-0.3/color-theme-zenburn.el")
 (load-file "~/.emacs.d/vendor/solarized/emacs-colors-solarized/color-theme-solarized.el")
 
+; color theme is global
+(setq color-theme-is-global t)
+
 ; select the colorscheme
-(if window-system
-    (color-theme-solarized-dark)
-    (color-theme-zenburn))
+(color-theme-solarized-dark)
 
 ; set the default font
 (set-default-font "Meslo LG M DZ-15")

e03771da1675c655ef0fa4ff15ce26b75c918bca
Petar Radosevic <petar@wunki.org>
2011-12-08 21:45:34 +0100

    Emacs theming.

--- emacs/wunki/theme.el
+++ emacs/wunki/theme.el
@@ -1,9 +1,10 @@
 ;(load-file "~/.emacs.d/elpa/color-theme-github-0.0.3/color-theme-github.elc")
-(load-file "~/.emacs.d/elpa/color-theme-twilight-0.1/color-theme-twilight.elc")
 ;(load-file "~/.emacs.d/elpa/color-theme-wombat+-0.0.2/color-theme-wombat+.elc")
 ;(load-file "~/.emacs.d/elpa/color-theme-zenburn-0.3/color-theme-zenburn.el")
 ;(load-file "~/.emacs.d/elpa/color-theme-railscasts-0.0.2/color-theme-railscasts.elc")
 ;(load-file "~/.emacs.d/elpa/color-theme-ir-black-1.0.1/color-theme-ir-black.elc")
+
+(load-file "~/.emacs.d/elpa/color-theme-twilight-0.1/color-theme-twilight.elc")
 
 ; color theme is global
 (setq color-theme-is-global t)
@@ -14,5 +15,5 @@
 ; set the default font
 ; fonts on the mac are a bit smaller
 (if (eq system-type 'darwin)
-  (set-default-font "Consolas-18")
-  (set-default-font "Consolas-15"))
+  (set-default-font "Consolas-19")
+  (set-default-font "Consolas-16"))

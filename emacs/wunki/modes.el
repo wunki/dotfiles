f6e8c06db090dd6aabd341b809c9d100dab76e9b
Petar Radosevic <petar@wunki.org>
2012-03-27 21:06:41 +0200

    Updated submodules and Haskell simple indent

--- emacs/wunki/modes.el
+++ emacs/wunki/modes.el
@@ -34,7 +34,7 @@
 ; haskell mode
 (load "~/.emacs.d/vendor/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 ; po-mode
 (setq auto-mode-alist 

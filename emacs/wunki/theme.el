26e3d346d3a79d3e74b13a8bad3c23547cdee7ec
Petar Radosevic <petar@wunki.org>
2012-08-22 16:02:13 +0200

    Merge with Arch Linux

--- emacs/wunki/theme.el
+++ emacs/wunki/theme.el
@@ -2,7 +2,7 @@
 (if (>= emacs-major-version 24)
   (progn
     (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-    (load-theme 'solarized-dark))
+    (load-theme 'zenburn))
   (progn
     (require 'color-theme)
     (color-theme-tomorrow-night)))

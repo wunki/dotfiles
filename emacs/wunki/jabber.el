f1a87675d674feda1e3cca01c5b8a9bf039081e1
Petar Radosevic <petar@wunki.org>
2012-09-21 12:46:56 +0200

    Server changes

--- emacs/wunki/jabber.el
+++ emacs/wunki/jabber.el
@@ -1,12 +1,17 @@
+;; default nicknames
+(setq jabber-muc-default-nicknames 
+      `((,jabber-doo-username . "Petar Radosevic")
+        (,jabber-bp-username . "Petar Radosevic")))
+
+;; jabber accounts
 (setq jabber-account-list
     `((,jabber-doo-username
        (:network-server . "im.doo.net")
+       (:password . ,jabber-doo-password))
+      (,jabber-doo-username
+       (:network-server . "conference.im.doo.net")
        (:password . ,jabber-doo-password))
       (,jabber-bp-username
        (:password . ,jabber-bp-password)
        (:network-server . "talk.google.com")
        (:connection-type . ssl))))
-
-(setq jabber-muc-default-nicknames 
-      `((,jabber-doo-username . "Petar Radosevic")
-        (,jabber-bp-username . "Petar Radosevic")))

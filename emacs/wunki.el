a84d755fa4535c85ec51094ce98017a99bdf821c
Petar Radosevic <petar@wunki.org>
2013-01-21 19:40:12 +0100

    Vim updates.

--- emacs/wunki.el
+++ emacs/wunki.el
@@ -35,7 +35,7 @@
 (load "wunki/modes")      ; settings for specific modes
 (load "wunki/theme")      ; set the theme and font
 (load "wunki/temp_files") ; temporary files
-;(load "wunki/org")        ; org-mode
+(load "wunki/org")        ; org-mode
 (load "wunki/magit")      ; magit
 (load "wunki/shell")      ; shell mode
 (load "wunki/lisp")       ; lisp languages
--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,15 @@ alias rmpyc "find . -name '*.pyc' | xargs rm"
 alias gs "git status --ignore-submodules=dirty"
 alias gp "git push origin master"
 alias gf "git pull origin master"
+alias l1 'tree --dirsfirst -ChFL 1'
+alias l2 'tree --dirsfirst -ChFL 2'
+alias l3 'tree --dirsfirst -ChFL 3'
 
 # Environment variables
 set -g -x fish_greeting ""
 
 set -g -x EDITOR vim
+
 function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
     if test -d $argv[1]
         if not contains $argv[1] $PATH
@@ -22,5 +26,12 @@ prepend_to_path "/bin"
 prepend_to_path "/usr/bin"
 prepend_to_path "/usr/local/bin"
 prepend_to_path "/usr/local/sbin"
+
+# Python
+set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
+set -g -x WORKON_HOME "$HOME/.virtualenvs"
+. ~/.config/fish/virtualenv.fish
+
+# Haskell
 prepend_to_path "$HOME/bin"
 prepend_to_path "$HOME/.cabal/.bin"

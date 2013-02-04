8152bba264e51149293724424a4660442104e414
Petar Radosevic <petar@wunki.org>
2013-02-04 12:14:07 +0100

    Checkmark for clean git status

--- fish/config.fish
+++ fish/config.fish
@@ -48,6 +48,7 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
 # Status Chars
+set __fish_git_prompt_char_upstream_equal '✓'
 set __fish_git_prompt_char_dirtystate '⚡'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'

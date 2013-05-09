c317660c65cd6a749e56c8b97a8339b2b738f368
Petar Radosevic <petar@wunki.org>
2013-05-09 21:41:53 +0200

    Switch default browser.

--- emacs/wunki/custom.el
+++ emacs/wunki/custom.el
@@ -8,9 +8,9 @@
 (setq user-full-name "Petar Radosevic")
 (setq user-mail-address "petar@wunki.org")
 
-;; conkeror as browser
+;; chromium as browser
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "/home/wunki/bin/conk")
+      browse-url-generic-program "chromium")
 
 ;; unicode
 (set-terminal-coding-system 'utf-8)

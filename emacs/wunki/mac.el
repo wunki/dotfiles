59cfceace6510ab5e83dafb46e0dcba4a25894e2
Petar Radosevic <petar@wunki.org>
2013-07-10 17:44:46 +0200

    Don't use native fullscreen on the Mac.

--- emacs/wunki/mac.el
+++ emacs/wunki/mac.el
@@ -1,2 +1,14 @@
-;; settings only used on a Mac
-(global-set-key (kbd "<f3>") 'ns-toggle-fullscreen)
+;;
+;; Mac only settings.
+;;
+
+;; use the old way of toggling to fullscreen
+(setq ns-use-native-fullscreen nil)
+(global-set-key (kbd "<f3>") 'toggle-frame-fullscreen)
+(set-face-attribute 'default nil :height 160)
+
+;; browser
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+;; extend exec-path for homebrew
+(setq exec-path (append exec-path '("/usr/local/bin")))

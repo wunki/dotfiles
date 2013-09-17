3659a9d827a703f97a8d96c7da59ff12a9836c9e
Petar Radosevic <petar@wunki.org>
2013-09-17 09:27:22 +0200

    Extra line below commit message now default in Magit.

--- emacs/wunki-magit.el
+++ emacs/wunki-magit.el
@@ -1,11 +1,5 @@
 (require 'magit)
 
-;; Add an extra newline to separate commit message from git commentary
-(defun magit-commit-mode-init ()
-  (when (looking-at "\n")
-    (open-line 1)))
-(add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
-
 ;; close popup when commiting
 (defadvice git-commit-commit (after delete-window activate)
   (delete-window))

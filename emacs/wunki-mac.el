8a953c5a23eb0a32dd49076f90379796e232c043
Petar Radosevic <petar@wunki.org>
2013-09-26 09:34:50 +0200

    Move some settings around.

--- emacs/wunki-mac.el
+++ emacs/wunki-mac.el
@@ -1,13 +1,9 @@
 ;; mac only settings.
 ;; use the old way of toggling to fullscreen
 (setq ns-use-native-fullscreen nil)
-(global-set-key (kbd "<f3>") 'toggle-frame-fullscreen)
 (set-face-attribute 'default nil :height 160)
 
 ;; browser
 (setq browse-url-browser-function 'browse-url-default-macosx-browser)
-
-;; extend exec-path for homebrew
-(setq exec-path (append exec-path '("/usr/local/bin")))
 
 (provide 'wunki-mac)
--- emacs/wunki-shell.el
+++ emacs/wunki-shell.el
@@ -2,5 +2,6 @@
 (add-to-list 'exec-path "/home/wunki/bin/")
 (add-to-list 'exec-path "/home/wunki/.local/bin/")
 (add-to-list 'exec-path "/home/wunki/.cabal/bin/")
+(setq exec-path (append exec-path '("/usr/local/bin")))
 
 (provide 'wunki-shell)

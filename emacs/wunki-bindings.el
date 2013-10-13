756b8a8004c918a2fa96a18b888319e630ab04d3
Petar Radosevic <petar@wunki.org>
2013-10-13 13:13:43 +0200

    Better bindings for multiple cursors.

--- emacs/wunki-bindings.el
+++ emacs/wunki-bindings.el
@@ -84,9 +84,9 @@
 (global-set-key (kbd "C-c N") 'windmove-down)
 
 ;; multiple markers
-(global-set-key (kbd "C-'") 'mc/edit-lines)
-(global-set-key (kbd "C-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-,") 'mc/mark-all-like-this-dwim)
+(global-set-key (kbd "C-!") 'mc/edit-lines)
+(global-set-key (kbd "C-@") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-#") 'mc/mark-all-like-this-dwim)
 
 ;; moving around
 (global-set-key (kbd "C-M-a") 'ace-jump-mode)

e78f2b6f0dd4b27ca14e6a7829f0a73a9a86e71f
Petar Radosevic <petar@wunki.org>
2013-10-19 09:45:55 +0200

    Change names to cider.

--- emacs/wunki-lisp.el
+++ emacs/wunki-lisp.el
@@ -1,5 +1,8 @@
 ;; lisp languages (mainly clojure)
 
+;; tmp fix
+(require 'cider)
+
 ;; enable this on lisp languages
 (defun lisp-coding-defaults ()
   (smartparens-strict-mode 1)
@@ -13,59 +16,35 @@
 ;; lisp implementation is SBCL
 (setq inferior-lisp-program "/usr/bin/sbcl --noinform")
 
-;; nrepl
-(setq nrepl-port "4001")
-(setq nrepl-buffer-name-show-port t)
-(setq nrepl-hide-special-buffers t)
-
-;; eldoc
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-
 ;; slime
 (slime-setup '(slime-repl))
-
-;; slime auto-completion
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
 
-;; nrepl autocompletion
-(add-hook 'nrepl-repl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-repl-mode))
+;; cider
+(setq nrepl-port "4001")
+(setq nrepl-buffer-name-show-port t)
+(setq nrepl-hide-special-buffers nil)
+(setq cider-auto-select-error-buffer t)
 
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'subword-mode)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+
+;; use tab to trigger auto-complete
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-(defun nrepl-connection-infos (connection-buffer)
-  (with-current-buffer (get-buffer connection-buffer)
-    nrepl-endpoint))
-
-(defun nrepl-current-connection-infos ()
-  (nrepl-connection-infos (nrepl-current-connection-buffer)))
-
-(defun nrepl-rotate-connection-list (connection-list)
-  (append (rest connection-list)
-          (list (first connection-list))))
-
-(defun nrepl-show-current-connection ()
-  (apply #'message
-         "Active nREPL connection: %s:%s"
-         (nrepl-current-connection-infos)))
-
-(defun nrepl-switch-to-next-connection ()
-  (interactive)
-  (setq nrepl-connection-list
-        (nrepl-rotate-connection-list nrepl-connection-list))
-  (nrepl-show-current-connection))
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; key bindings
-(global-set-key (kbd "C-c C-x n") 'nrepl-switch-to-next-connection)
+(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 (eval-after-load 'clojure-mode
   '(define-key clojure-mode-map (kbd "M-q") 'sp-indent-defun))
 

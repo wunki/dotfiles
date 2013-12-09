e50b5fd2b7715d7b394f76359c86f964c7eaa161
Petar Radosevic <petar@wunki.org>
2013-12-09 10:09:34 +0100

    Enable uniquify.

--- emacs/wunki-modes.el
+++ emacs/wunki-modes.el
@@ -14,6 +14,13 @@
 ;; deletes region when starting typing
 (pending-delete-mode t)
 
+;; improve buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t)    ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
 ;; auto revert changes on disk
 (global-auto-revert-mode t)
 

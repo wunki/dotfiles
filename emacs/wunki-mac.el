4256fb3a66b43bb1a9461e03f52ab58c0adcfae0
Petar Radosevic <petar@wunki.org>
2014-06-01 22:33:38 +0200

    Fira Mono font.

--- emacs/wunki-mac.el
+++ emacs/wunki-mac.el
@@ -1,12 +1,15 @@
 ;; mac only settings.
 ;; use the old way of toggling to fullscreen
-(set-frame-font "Menlo-14")
-(add-to-list 'default-frame-alist '(font . "Ubuntu Mono 16"))
+(set-frame-font "Fira Mono-13")
+(add-to-list 'default-frame-alist '(font . "Fira Mono 13"))
 
 ;; browser
 (setq browse-url-browser-function 'browse-url-default-macosx-browser)
 
 ;; set the correct path on the Mac
-(exec-path-from-shell-initialize)
+(when (file-name-nondirectory (getenv "SHELL")) "fish"
+   (setq path-separator " ")
+   (exec-path-from-shell-initialize)
+   (setq path-separator ":"))
 
 (provide 'wunki-mac)

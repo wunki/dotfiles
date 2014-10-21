7edba391510f1a5f8a61741d36a07caebcebe2d9
Petar Radosevic <petar@wunki.org>
2014-10-21 14:28:55 +0200

    Bigger fonts!

--- emacs/wunki/wunki-mac.el
+++ emacs/wunki/wunki-mac.el
@@ -1,7 +1,7 @@
 ;; mac only settings.
 ;; use the old way of toggling to fullscreen
-(set-frame-font "Fira Mono-14")
-(add-to-list 'default-frame-alist '(font . "Fira Mono 14"))
+(set-frame-font "Fira Mono-16")
+(add-to-list 'default-frame-alist '(font . "Fira Mono 16"))
 
 ;; browser
 (setq browse-url-browser-function 'browse-url-default-macosx-browser)

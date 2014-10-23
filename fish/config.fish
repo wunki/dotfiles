a12ef578eff389289ad24f90f0fefa6d157b98c2
Petar Radosevic <petar@wunki.org>
2014-10-23 09:46:22 +0200

    Setup hub.

--- fish/config.fish
+++ fish/config.fish
@@ -58,6 +58,10 @@ set -x TERM 'screen-256color'
 # autojump
 . ~/.config/fish/autojump.fish
 
+if test -f "/usr/local/bin/hub"
+   function git; hub $argv; end
+end
+
 function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
     if test -d $argv[1]
         if not contains $argv[1] $PATH

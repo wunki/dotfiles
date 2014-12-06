fb789eea5d5275ce21e34d812d969c47ba7812b3
Petar Radosevic <petar@wunki.org>
2014-12-06 13:32:07 +0100

    Use Zenburn theme when in Terminal.

--- emacs/wunki/wunki-theme.el
+++ emacs/wunki/wunki-theme.el
@@ -1,9 +1,11 @@
 ;; load theme and don't ask if it's save.
-(load-theme 'sanityinc-tomorrow-blue t)
+(if (display-graphic-p)
+    (load-theme 'sanityinc-tomorrow-blue t)
+  (load-theme 'zenburn t))
 
 (set-frame-font "Ubuntu Mono")
 (add-to-list 'default-frame-alist '(font . "Ubuntu Mono"))
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :height 120)
 
 (defun toggle-dark-light-theme ()
   "Switch between dark and light theme."

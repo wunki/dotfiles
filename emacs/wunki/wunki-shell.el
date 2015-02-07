126258d24b4329750f4be2c3a71baf4a062164ca
Petar Radosevic <petar@wunki.org>
2015-02-07 09:29:55 +0000

    Also use emacsclient on Unix.

--- emacs/wunki/wunki-shell.el
+++ emacs/wunki/wunki-shell.el
@@ -6,6 +6,9 @@
 ;; use fish as the default shell
 (setq explicit-shell-file-name (locate-file "fish" exec-path))
 
+;; emacsclient as editor for Magit
+(setenv "EDITOR" "emacsclient")
+
 ;; copy some vars over
 (exec-path-from-shell-copy-env "GOPATH")
 (exec-path-from-shell-copy-env "PYTHONPATH")

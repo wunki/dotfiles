f78db54cf1659cbd00e9c6e864f21d8857a67b9d
Petar Radosevic <petar@wunki.org>
2015-03-14 12:53:11 +0000

    Binding for ace-jump mode.

--- emacs/wunki/wunki-bindings.el
+++ emacs/wunki/wunki-bindings.el
@@ -92,7 +92,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; moving around
-(global-set-key (kbd "C-c j") 'ace-jump-mode)
+(global-set-key (kbd "C-x g") 'ace-jump-mode)
 (global-set-key (kbd "C-c C-i") 'idomenu)
 
 ;; quickly edit files

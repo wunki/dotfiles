c279545fa4f139661fb0fde47de8113f87330e70
Petar Radosevic <petar@wunki.org>
2015-06-05 11:44:48 +0200

    Fullframe for Emacs.

--- emacs/wunki/wunki-git.el
+++ emacs/wunki/wunki-git.el
@@ -3,19 +3,9 @@
 
 (global-set-key (kbd "C-c g") 'magit-status)
 
-;; store windows when invoking magit
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-;; restore windows when quiting
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (when (get-register :magit-fullscreen)
-    (jump-to-register :magit-fullscreen)))
+(require 'fullframe)
+(after-load 'magit
+  (fullframe magit-status magit-mode-quit-window))
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 

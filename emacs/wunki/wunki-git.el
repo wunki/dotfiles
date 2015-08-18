ae957ffa88d95d1fedcaf4074becf049233ac9fd
Petar Radosevic <petar@wunki.org>
2015-08-18 21:02:44 +0200

    Magit changes

--- emacs/wunki/wunki-git.el
+++ emacs/wunki/wunki-git.el
@@ -2,7 +2,7 @@
 (require 'magit)
 
 (setq magit-push-always-verify nil) ; don't ask me to confirm the push
-(setq magit-diff-auto-show '(stage-all log-oneline log-select blame-follow))
+(setq magit-diff-auto-show '(stage-all log-oneline log-select blame-follow)) ; don't show diff on commit
 
 (global-set-key (kbd "C-c g") 'magit-status)
 

7ad3e694d194b5e3dfae8f383dae7accdeb71f9a
Petar Radosevic <petar@wunki.org>
2015-10-19 09:13:31 +0000

    Back to Zenburn.

--- emacs/wunki/wunki-theme.el
+++ emacs/wunki/wunki-theme.el
@@ -4,7 +4,7 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; load theme and don't ask if it's save.
-(load-theme 'sanityinc-tomorrow-blue t)
+(load-theme 'zenburn t)
 
 (set-frame-font "Fira Mono")
 (add-to-list 'default-frame-alist '(font . "Fira Mono"))

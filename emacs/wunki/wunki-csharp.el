4c09da56db3c386aaba1529a9a5afabe672aa2ae
Petar Radosevic <petar@wunki.org>
2016-04-06 13:59:13 +0200

    Disable flycheck for CSharp.

--- emacs/wunki/wunki-csharp.el
+++ emacs/wunki/wunki-csharp.el
@@ -1,6 +1,5 @@
 ;; modes which improve the experience
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
-(add-hook 'csharp-mode-hook 'flycheck-mode)
 (add-hook 'csharp-mode-hook 'yas-minor-mode)
 (add-hook 'csharp-mode-hook 'eldoc-mode)
 

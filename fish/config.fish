c61200cb8acbf72de361a008429d276c79812eba
Petar Radosevic <petar@wunki.org>
2016-06-02 09:27:09 +0200

    True colors in terminal

--- fish/config.fish
+++ fish/config.fish
@@ -66,7 +66,7 @@ set -x LANG 'en_US.UTF-8'
 set -x LC_ALL 'en_US.UTF-8'
 set -x EDITOR 'nvim'
 set -x VISUAL 'nvim'
-set -x TERM 'screen-256color'
+set -x TERM 'xterm-256color'
 set -x XDG_DATA_HOME {$HOME}/.local/share
 
 # secret environment vars

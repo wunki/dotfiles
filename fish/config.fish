c61afee208880e9d3e5e6564e7d53ee0cd3f5aa5
Petar Radosevic <petar@wunki.org>
2016-06-19 11:42:29 +0200

    Tmux cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -177,9 +177,7 @@ end
 prepend_to_path "/Applications/Racket v6.1.1/bin"
 
 # rubygems
-prepend_to_path "$HOME/.gem/ruby/2.0.0/bin"
-prepend_to_path "$HOME/.gem/ruby/2.1/bin"
-prepend_to_path "$HOME/.gem/ruby/2.2.0/bin"
+prepend_to_path "$HOME/.gem/ruby/2.3.0/bin"
 
 # android
 prepend_to_path "/opt/android-sdk/tools"

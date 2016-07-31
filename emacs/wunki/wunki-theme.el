2c852f9f580b8278bff3e4ea5e682164b9ae3a4b
Petar Radosevic <petar@wunki.org>
2016-07-31 20:43:25 +0200

    Source Code Pro font.

--- emacs/wunki/wunki-theme.el
+++ emacs/wunki/wunki-theme.el
@@ -7,8 +7,8 @@
 (load-theme 'sanityinc-tomorrow-blue t)
 
 ;; font face
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-14"))
-(set-frame-font "Source Code Pro 14" nil t)
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-16"))
+(set-frame-font "Source Code Pro 16" nil t)
 (if (mac?)
     (setq-default line-spacing 2))
 

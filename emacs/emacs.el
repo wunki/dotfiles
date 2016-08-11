15e5cd262a355fb5a99088c42ebe9c2e5ecddbb0
Petar Radosevic <petar@wunki.org>
2016-08-11 09:29:20 +0200

    Move ELPA configuration.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,3 +1,12 @@
+;; package repositories
+(setq package-archives '(("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("elpy" . "https://jorgenschaefer.github.io/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/wunki")
 
 (load "wunki-init")
--- emacs/wunki/wunki-init.el
+++ emacs/wunki/wunki-init.el
@@ -6,14 +6,6 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; package repositories
-(setq package-archives '(("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("elpy" . "https://jorgenschaefer.github.io/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-
 ;; my packages
 (defvar wunki-packages
   '(magit

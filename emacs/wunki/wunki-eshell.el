68987bf15e1adde9162b99bfff2c2879964163f1
Petar Radosevic <petar@wunki.org>
2016-11-18 10:02:29 +0100

    Eshell bindings.

--- emacs/wunki/wunki-eshell.el
+++ emacs/wunki/wunki-eshell.el
@@ -1,6 +1,3 @@
-;; Define a keybinding to get to your eshell quickly.
-(global-set-key (kbd "C-c C-e") 'eshell)
-
 ;; Visual commands are commands which require a proper terminal.
 ;; eshell will run them in a term buffer when you invoke them.
 (setq eshell-visual-commands
@@ -14,13 +11,12 @@
 
 ;; Disable line highligthing
 (add-hook 'eshell-mode-hook (lambda ()
-                              (setq-local global-hl-line-mode
-                                          nil)))
+                              (setq-local global-hl-line-mode nil)
+                              (define-key eshell-mode-map (kbd "M-p") 'ace-window)))
 
 (setq eshell-cmpl-cycle-completions nil)
 
 ;; quickly fire up e-shell.
-(global-set-key (kbd "M-RET") 'eshell)
-
+(global-set-key (kbd "C-c C-e") 'eshell)
 
 (provide 'wunki-eshell)

e9fa3c96f1dfe89e44ef428ddc9c32991cbef19b
Petar Radosevic <petar@wunki.org>
2016-11-20 17:15:17 +0100

    A

--- fish/config.fish
+++ fish/config.fish
@@ -184,6 +184,7 @@ prepend_to_path "/Applications/Racket v6.1.1/bin"
 
 # rubygems
 prepend_to_path "$HOME/.gem/ruby/2.0.0/bin"
+prepend_to_path "$HOME/.gem/ruby/2.3.0/bin"
 
 # android
 prepend_to_path "/opt/android-sdk/tools"

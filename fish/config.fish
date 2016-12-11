b14d50eadd6426f4e6965282e5b5aada678698d1
Petar Radosevic <petar@wunki.org>
2016-12-11 19:02:47 +0100

    Don't set the TERM in fish

--- fish/config.fish
+++ fish/config.fish
@@ -66,8 +66,6 @@ set -x LANG 'en_US.UTF-8'
 set -x LC_ALL 'en_US.UTF-8'
 set -x EDITOR 'nvim'
 set -x VISUAL 'nvim'
-set -x TERMINFO {$HOME}/.terminfo
-set -x TERM 'xterm-256color'
 set -x XDG_DATA_HOME {$HOME}/.local/share
 
 # secret environment vars

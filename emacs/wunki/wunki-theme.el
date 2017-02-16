6aec2b14d2ea50b40a6b5b30e8797e4583fabe4d
Petar Radosevic <petar@degreed.com>
2017-02-16 19:42:25 +0100

    Theme.

--- emacs/wunki/wunki-theme.el
+++ emacs/wunki/wunki-theme.el
@@ -4,13 +4,13 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; load theme and don't ask if it's save.
-(load-theme 'zenburn t)
+(load-theme 'sanityinc-tomorrow-blue t)
 
 ;; font
-(let ((font "Operator Mono Light")
+(let ((font "Source Code Pro")
       (size (if (mac?) 15 9)))
-  (add-to-list 'default-frame-alist (cons 'font (format "%s %s" font size)))
-  (set-frame-font (format "%s %s" font size) nil t))
+  (add-to-list 'default-frame-alist (cons 'font (format "%s-%s" font size)))
+  (set-frame-font (format "%s-%s" font size) nil t))
 
 ;; increase space between lines on the mac.
 (if (mac?)

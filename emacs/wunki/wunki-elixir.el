ecee053a8483f67ca85bc3be4dc39811423b5f5c
Petar Radosevic <petar@wunki.org>
2017-04-10 16:23:12 +0200

    Elixir on Emacs.

--- emacs/wunki/wunki-elixir.el
+++ emacs/wunki/wunki-elixir.el
@@ -1,2 +1,12 @@
+(require 'elixir-mode)
+
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+         :when '(("SPC" "RET"))
+         :actions '(insert navigate))
+  (sp-local-pair "do" "end"
+         :when '(("SPC" "RET"))
+         :post-handlers '(sp-ruby-def-post-handler)
+         :actions '(insert navigate)))
 
 (provide 'wunki-elixir)
--- emacs/wunki/wunki-init.el
+++ emacs/wunki/wunki-init.el
@@ -63,6 +63,7 @@
     paradox
 
     ;; elixir
+    elixir-mode
     alchemist
 
     ;; rust

196146f975f9653d839bfd56a08396d67be9f6c9
Petar Radosevic <petar@petar.dev>
2021-04-15 14:42:30 +0200

    Update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,38 +1,61 @@
-# git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'git'
-set __fish_git_prompt_color_branch yellow
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
 
-# status chars
-set __fish_git_prompt_char_upstream_equal '✓'
-set __fish_git_prompt_char_dirtystate '✚'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
 
-# fish prompt
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
 function fish_prompt
-  # we need to set this prompt to make TRAMP work in Emacs.
-  if test $TERM = "dumb"
-     echo "> "
-  else
-    set last_status $status
+  set -l last_status $status
 
-    # Hostname
-    if test -n "$SSH_CONNECTION"
-      set_color blue
-      printf '%s | ' (hostname -s)
-    end
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
 
-    # CWD
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
+  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
 
-    # Git
-    set_color normal
-    printf '%s ' (__fish_git_prompt)
-    set_color normal
+  # Output the prompt, left to right
+
+  # Add a newline before new prompts
+  echo -e ''
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
   end
+
+  # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '(' $yellow $git_branch "±" $normal ')'
+    else
+      set git_info '(' $green $git_branch $normal ')'
+    end
+    echo -n -s ' · ' $git_info $normal
+  end
+
+  set -l prompt_color $red
+  if test $last_status = 0
+    set prompt_color $normal
+  end
+
+  # Terminate with a nice prompt char
+  echo -e ''
+  echo -e -n -s $prompt_color 'λ ' $normal
 end

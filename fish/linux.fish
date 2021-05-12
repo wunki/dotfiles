ea72bd1b4e15e480a0085c1d9ff20a1e9c5892bd
Petar Radosevic <petar@petar.dev>
2021-05-12 11:31:37 +0200

    Using WSLG

--- fish/linux.fish
+++ fish/linux.fish
@@ -1,12 +1,12 @@
 abbr paco 'sudo pacman -Rs (pacman -Qqtd)'
-abbr sudo 'lsof -PiTCP -sTCP:LISTEN'
+abbr ports 'sudo lsof -PiTCP -sTCP:LISTEN'
 
 prepend_to_path "/mnt/c/Program Files/Docker/Docker/resources/bin"
 prepend_to_path "/mnt/c/Users/Petar Radosevic/AppData/Local/Programs/Microsoft VS Code/bin"
 
 # Configuration specific to WSL2 Linux
 if string match -q "*microsoft*" (uname -a)
-  set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+  # set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
   set -x GDK_SCALE 0.5
   set -x GDK_DPI_SCALE 2
   set -x PYTHON_KEYRING_BACKEND keyring.backends.null.Keyring

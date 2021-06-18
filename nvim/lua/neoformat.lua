23c2953bd0f1fd8cdb906e07f8f7968812510433
Petar Radosevic <petar@petar.dev>
2021-06-18 14:21:45 +0200

    Many changes

---
+++ nvim/lua/neoformat.lua
@@ -0,0 +1,7 @@
+-- Automatically format Rust buffers
+vim.api.nvim_exec([[
+augroup fmt
+  autocmd!
+  autocmd BufWritePost *.rs try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+augroup END
+]], true)

84e40da18dbdae9cc7997e5f4d2a61ef1fb957d8
Petar Radosevic <petar@petar.dev>
2021-07-02 08:55:01 +0200

    chore: gitsigns configuration

--- neovim/lua/gitsigns-nvim.lua
+++ neovim/lua/gitsigns-nvim.lua
@@ -1,7 +1,7 @@
 require("gitsigns").setup {
     signs = {
-        add = {hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr"},
-        change = {hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr"},
+        add = {hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr"},
+        change = {hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr"},
         delete = {hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr"},
         topdelete = {hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr"},
         changedelete = {hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr"}

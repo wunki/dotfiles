dc23b5160dfe887b01d2d8510f544b4c8df64934
Petar Radosevic <petar@petar.dev>
2021-07-06 22:26:01 +0200

    refactor: every plugin it's own conf

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,29 +1,13 @@
 require "plugins"
 require "options"
 require "utils"
-require "top-bufferline"
+require "mappings"
 
-local g = vim.g
-
-g.mapleader = " "
-g.auto_save = false
-
--- colorscheme related stuff
-g.wunki_theme = "gruvbox"
+-- colors
+vim.g.wunki_theme = "gruvbox"
 local base16 = require "base16"
 base16(base16.themes["nvchad-softgruv"], true)
 
+-- custom colorss
 require "highlights"
-require "mappings"
-require "file-icons"
-require "statusline"
 
--- hide line numbers , statusline in specific buffers!
-vim.api.nvim_exec(
-    [[
-   au BufEnter term://* setlocal nonumber
-   au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif
-   au BufEnter term://* set laststatus=0 
-]],
-    false
-)

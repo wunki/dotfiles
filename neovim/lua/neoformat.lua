19ba8674be0e49d8cbd2ca5fd4b6789c0c828494
Petar Radosevic <petar@petar.dev>
2021-07-06 17:05:16 +0200

    refactor: copy in nvchad vim configuration

--- neovim/lua/neoformat.lua
+++
@@ -1,7 +0,0 @@
--- Automatically format Rust buffers
-vim.api.nvim_exec([[
-augroup fmt
-  autocmd!
-  autocmd BufWritePre *.rs try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
-augroup END
-]], true)

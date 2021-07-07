f9733374b5a0a0c6d75c98b9f6fd73a0f8c09d70
Petar Radosevic <petar@petar.dev>
2021-07-07 15:22:13 +0200

    feat: set colorscheme and format lua

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 M.config = function()
-    local ts_config = require("nvim-treesitter.configs")
+   local ts_config = require('nvim-treesitter.configs')
 
-    ts_config.setup {
-        ensure_installed = {
-            "javascript",
-            "html",
-            "css",
-            "bash",
-            "lua",
-            "json",
-            "python"
-            -- "rust",
-            -- "go"
-        },
-        highlight = {
-            enable = true,
-            use_languagetree = true
-        }
-    }
+   ts_config.setup({
+      ensure_installed = {
+         'javascript',
+         'html',
+         'css',
+         'bash',
+         'lua',
+         'json',
+         'python',
+         -- "rust",
+         -- "go"
+      },
+      highlight = {
+         enable = true,
+         use_languagetree = true,
+      },
+   })
 end
 
 return M

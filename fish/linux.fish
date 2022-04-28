c2f34c3e00fdb8c7d9bafc86a3814c2c4e52c3af
Petar Radosevic <petar@petar.dev>
2022-04-28 21:00:34 +0200

    chore: disable keychain for now in WSL

--- fish/linux.fish
+++ fish/linux.fish
@@ -1,11 +1,11 @@
-set -x TERM "xterm-kitty"
-abbr ssh 'env TERM=xterm-256color ssh'
+# set -x TERM "xterm-kitty"
+# abbr ssh 'env TERM=xterm-256color ssh'
 
 # Configuration specific to WSL2 Linux
 if string match -q "*microsoft*" (uname -a)
     set -x PYTHON_KEYRING_BACKEND keyring.backends.null.Keyring
     
-    keychain --eval --quiet --agents ssh id_rsa | source
+    # keychain --eval --quiet --agents ssh id_rsa | source
 
     fish_add_path -aP "/mnt/c/Program Files/Docker/Docker/resources/bin"
     fish_add_path -aP "/mnt/c/Users/petar/AppData/Local/Programs/Microsoft VS Code/bin"

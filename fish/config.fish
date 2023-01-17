4f9d59be1d8f4133f6784d51f9d07b08f626f131
Petar Radosevic <petar@petar.dev>
2023-01-17 11:06:16 +0100

    fix: fix Fish slowness... it was Java again

--- fish/config.fish
+++ fish/config.fish
@@ -138,12 +138,3 @@ fish_add_path -aP $HOME/.docker/cli-plugins
 
 # ASDF: version manager for different languages
 test -f "$HOME/.asdf/asdf.fish" ; and source $HOME/.asdf/asdf.fish
-
-if type -q asdf
-    fish_add_path -aP (asdf where elixir)/.mix/escripts
-
-    if test -d ~/.asdf/plugins/java
-      . ~/.asdf/plugins/java/set-java-home.fish
-    end
-end
-

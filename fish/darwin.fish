9b479d31cc8ac0a0c03234dda94badc15181956a
Petar Radosevic <petar@petar.dev>
2023-01-17 10:49:03 +0100

    feat: clean up Fish shell

--- fish/darwin.fish
+++ fish/darwin.fish
@@ -1,3 +1,5 @@
+set -U brew_prefix (brew --prefix)
+
 # Setup homebrew
 if status is-interactive
   eval (/opt/homebrew/bin/brew shellenv)
@@ -5,19 +7,17 @@ end
 
 # Mac specific paths
 fish_add_path -aP /opt/homebrew/bin
-fish_add_path -aP (brew --prefix)/opt/node@14/bin
-fish_add_path -aP (brew --prefix)/opt/python@3.9/libexec/bin
+fish_add_path -aP $brew_prefix/opt/node@14/bin
+fish_add_path -aP $brew_prefix/opt/python@3.9/libexec/bin
+fish_add_path -aP /Applications/Postgres.app/Contents/Versions/latest/bin
 
 # Emacs
 fish_add_path -aP /Applications/Emacs.app/Contents/MacOS
 fish_add_path -aP /Applications/Emacs.app/Contents/MacOS/bin
 
-# set -x TERMINFO "/Applications/kitty.app/Contents/Resources/kitty/terminfo"
-set -x SHELL fish
-set -x PROJECT_DIR {$HOME}/Code
-set -x OPENSSL_INCLUDE_DIR (brew --prefix)/opt/openssl/include
-set -x OPENSSL_LIB (brew --prefix)/opt/openssl/lib
-set -x OPENSSL_ROOT_DIR (brew --prefix)/opt/openssl
+set -x OPENSSL_INCLUDE_DIR $brew_prefix/opt/openssl/include
+set -x OPENSSL_LIB $brew_prefix/opt/openssl/lib
+set -x OPENSSL_ROOT_DIR $brew_prefix/opt/openssl
 
 function bup --description "Updates, upgrades and cleanes Homebrew"
     brew update

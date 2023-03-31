259f89594c982db7d854b5519af561fc4cf30519
Petar Radosevic <petar@petar.dev>
2023-03-31 06:41:13 +0200

    Put vterm in seperate configuration

--- fish/config.fish
+++ fish/config.fish
@@ -161,36 +161,7 @@ fish_add_path -aP $HOME/.docker/cli-plugins
 # ASDF: version manager for different languages
 test -f "$HOME/.asdf/asdf.fish" ; and source $HOME/.asdf/asdf.fish
 
-# Emacs VTERM configuration
-# This makes sure that the right escape sequences are sent.
+# VTerm in Emacs
 if [ "$INSIDE_EMACS" = 'vterm' ]
-    function vterm_printf;
-        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
-            # tell tmux to pass the escape sequences through
-            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-        else if string match -q -- "screen*" "$TERM"
-            # GNU screen (screen, screen-256color, screen-256color-bce)
-            printf "\eP\e]%s\007\e\\" "$argv"
-        else
-            printf "\e]%s\e\\" "$argv"
-        end
-    end
-
-    function clear
-        vterm_printf "51;Evterm-clear-scrollback";
-        tput clear;
-    end
-
-    function vterm_prompt_end;
-        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
-    end
-    functions --copy fish_prompt vterm_old_fish_prompt
-
-    function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-        # Remove the trailing newline from the original prompt. This is done
-        # using the string builtin from fish, but to make sure any escape codes
-        # are correctly interpreted, use %b for printf.
-        printf "%b" (string join "\n" (vterm_old_fish_prompt))
-        vterm_prompt_end
-    end
+    . $HOME/.config/fish/vterm.fish
 end

7f7689b71e2526d90dfa2eb7106eb1402910400c
Petar Radosevic <petar@petar.dev>
2026-02-17 17:46:07 +0100

    changes people, many changes

--- fish/conf.d/20-fzf-gondolin.fish
+++ fish/conf.d/20-fzf-gondolin.fish
@@ -1,6 +1,1 @@
-if status is-interactive; and type -q fzf
-    set -g fzf_history_opts \
-        --color=bg:-1,bg+:#1a1d23,fg:-1,fg+:#D0D3DA,hl:#CC95FF,hl+:#eebcbc \
-        --color=header:#FFBE78,info:#83AEF8,pointer:#83AEF8 \
-        --color=marker:#83AEF8,prompt:#FF7A77,spinner:#83AEF8
-end
+# fzf colors now handled by gondolin_apply

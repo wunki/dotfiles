99459b5b39415ae9aa85bf4c2a4863c04b822618
Petar Radosevic <petar@petar.dev>
2026-08-28 09:38:40 +0200

    chore(fish): use a chevron prompt marker

--- fish/conf.d/30-tide.fish
+++ fish/conf.d/30-tide.fish
@@ -3,8 +3,8 @@ if status is-interactive
     set -U tide_left_prompt_items context pwd git newline character
     set -U tide_right_prompt_items status cmd_duration jobs pulumi kubectl distrobox toolbox terraform aws nix_shell
 
-    set -U tide_character_icon λ
-    set -U tide_character_vi_icon_default λ
+    set -U tide_character_icon ›
+    set -U tide_character_vi_icon_default ›
     set -U tide_character_vi_icon_replace ▶
     set -U tide_character_vi_icon_visual V
 
